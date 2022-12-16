9d2fced4695f449f8bb904dcfaf8a38be043f312
NCLAN\dtc <duongthuychuong@gmail.com>
2022-12-16 18:01:00 +0700

    key

--- Desktop/REact_Course/React-project-chuong/gitHubUser/git-user/src/index.js
+++ Desktop/REact_Course/React-project-chuong/gitHubUser/git-user/src/index.js
@@ -5,6 +5,10 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
+// dev-jc62e0k1on4sbsux.us.auth0.com
+// uWDF7yhR0tLVsP3UExpWyY4ZgmbiPR8B
+// BwMWnQoddeQJ6Ult3WZxQBecfOi0QAH9c6jEVtHxc5L2-H7B_gi_J5mhfDKvXkCo
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
